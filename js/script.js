84f482852b32df93fc7ba30b2c07ee4368f8e681
JanneAM <janne.majasalmi@gmail.com>
2020-09-10 11:25:01 +0300

    jebbou

--- js/script.js
+++ js/script.js
@@ -6,13 +6,13 @@ var now = new Date();
 var sec = now.getSeconds();
 var min = now.getMinutes();
 var hr = now.getHours();
+var angle = 360 / 60;
 
 function moveArms() {
   sec += 1;
-  var deltaSec = (360 / 60) * sec;
-  var deltaMin = (min * 60) / 360 + deltaSec / 60;
-  //var deltaMin = deltaSec / 60;
-  var deltaHr = deltaMin / 60;
+  var deltaSec = angle * sec;
+  var deltaMin = angle * min + (deltaSec / 60) * angle;
+  var deltaHr = angle * 5 * hr + (deltaMin / 60) * angle * 5;
 
   SECONDHAND.style.transform = `rotate(${deltaSec}deg)`;
   MINUTEHAND.style.transform = `rotate(${deltaMin}deg)`;
